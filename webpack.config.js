00d027d83f6cb5a42dbb1942b9131f42f3f47809
yhci <93092282+yhci@users.noreply.github.com>
2023-05-09 12:48:46 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+const path = require('path');
+
 module.exports = {
   entry: './server.js', // 你的入口点
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/dist'
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify")
+    }
   }
 };
 
