8b6d826b3b7e7d5201ff2a748edf09630fa1b712
lqz2002 <82135379+lqz2002@users.noreply.github.com>
2023-05-09 10:19:08 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  entry: './server.js', // 你的入口点
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist'
-  }
+entry: './server.js', // 你的入口点
+output: {
+filename: 'bundle.js',
+path: __dirname + '/dist'
+},
+resolve: {
+fallback: {
+util: require.resolve('util/'),
+os: require.resolve('os-browserify/browser'),
+path: require.resolve('path-browserify')
+}
+}
 };
 
